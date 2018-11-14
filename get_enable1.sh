fc529ce34010be4d583830b7b6acc465f5159d46
pegeler <paulegeler@gmail.com>
2018-11-14 02:53:33 -0500

    Adding test to see if enable1.txt is already downloaded.

--- get_enable1.sh
+++ get_enable1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/dolph/dictionary/master/enable1.txt
+test -f enable1.txt || \
+  wget https://raw.githubusercontent.com/dolph/dictionary/master/enable1.txt
 
