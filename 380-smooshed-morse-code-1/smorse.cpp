19f55ed157924a5307c1559eef6a55776687961b
pegeler <32426645+pegeler@users.noreply.github.com>
2020-08-19 22:09:15 -0400

    formatting smorse.cpp

--- 380-smooshed-morse-code-1/smorse.cpp
+++ 380-smooshed-morse-code-1/smorse.cpp
@@ -7,15 +7,13 @@ Rcpp::StringVector smorse_cpp(Rcpp::StringVector words, Rcpp::StringVector code)
   Rcpp::StringVector out(n);
   out.names() = words;
 
-  for ( int i = 0; i < n; i++ )
-  {
-    for ( int j = 0; j < words[i].size(); j++ )
-    {
-      int c = words[i][j] - 0x61;
-      if ( c >= 0 && c < 26 )
-        out[i] += code(c);
+  for ( int i = 0; i < n; i++ ) {
+    for ( int j = 0; j < words[i].size(); j++ ) {
+      int c;
+      if ( (c = words[i][j]) >= 'a' && c <= 'z' )
+        out[i] += code[c - 'a'];
       else
-        Rcpp::stop("Character out of bounds: '%s'", words[i][j]);
+        Rcpp::stop("'%c' is not a lower case alphabetic character", c);
     }
   }
 
