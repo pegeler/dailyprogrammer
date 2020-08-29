e17b7fb00b7867b058ddda31ea478c8cd9b25412
pegeler <32426645+pegeler@users.noreply.github.com>
2020-08-29 02:21:48 -0400

    367 derangements in c

---
+++ 367-derangements/derangements.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+
+/**
+ * Compute the factorial
+ * (Don't need this)
+ */
+int64_t factorial(int64_t n)
+{
+  int64_t x = 1;
+  while (n > 1)
+    x *= n--;
+  return x;
+}
+
+/**
+ * Get number of derangements possible given |set| = n
+ */
+int64_t D(int64_t n)
+{
+  int64_t x = 0, y = 1;
+  int s = n & 1;
+  while (n > 1) {
+    if (s)
+      x -= y;
+    else
+      x += y;
+    y *= n--;
+    s = !s;
+  }
+  return x;
+}
+
+int main(int argc, char *argv[])
+{
+
+  if (argc == 2) {
+    printf("%ld\n", D(strtol(argv[1], NULL, 10)));
+  } else {
+    assert(D(1) == 0);
+    assert(D(2) == 1);
+    assert(D(3) == 2);
+    assert(D(4) == 9);
+    assert(D(5) == 44);
+    /* challenge input */
+    assert(D(6) == 265);
+    assert(D(9) == 133496);
+    assert(D(14) == 32071101049);
+    fprintf(stderr, "checks OK\n");
+  }
+
+  return 0;
+}
