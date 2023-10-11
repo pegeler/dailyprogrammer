8dab5f45f2726aaee073fac25f7ff052bcd533a8
pegeler <32426645+pegeler@users.noreply.github.com>
2023-10-11 17:42:51 -0400

    cleaning up 383 with some modern python idioms

--- 383-necklace-matching/bonus2.py
+++ 383-necklace-matching/bonus2.py
@@ -2,32 +2,40 @@
 # -*- coding: utf-8 -*-
 """
 383 Necklace Matching -- Bonus 2
+
+There is exactly one set of four words in the enable1 word list that
+all describe the same necklace. Find the four words.
+
 Got the idea to _canonicalize_ from `skeeto`, who did it in Golang.
 """
-from __future__ import print_function
-from sys import argv
+import sys
+from collections import defaultdict
 
 
 def canonicalize(s):
     strlen = len(s)
-    t = s*2
-    for i in range(1,strlen):
-        if s > t[i:(i+strlen)]:
-            s = t[i:(i+strlen)]
+    t = s * 2
+    for i in range(1, strlen):
+        if s > t[i:(i + strlen)]:
+            s = t[i:(i + strlen)]
     return s
 
 
-matches = dict()
-for line in open(argv[1], 'rt'):
-    l = line.strip()
-    canonical = canonicalize(l)
-    if len(canonical) >= 4:
-        try:
-            matches[canonical].append(l)
-        except KeyError:
-            matches.update({canonical: [l]})
+def find_matches(words):
+    matches = defaultdict(list)
+    for word in words:
+        canonical = canonicalize(word)
+        if len(canonical) >= 4:
+            matches[canonical].append(word)
+    return matches
 
-for v in matches.values():
-    if len(v) == 4:
-        print(v)
-        break
+
+def main(argv=None):
+    file_ = sys.argv[1] if argv is not None else 'enable1.txt'
+    with open(file_, 'rt') as fh:
+        matches = find_matches(x.strip() for x in fh)
+    print(next(filter(lambda x: len(x) == 4, matches.values())))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
