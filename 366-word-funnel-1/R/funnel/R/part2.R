4c1982adebfb7914375aaab02b98906850eec4e9
pegeler <32426645+pegeler@users.noreply.github.com>
2024-12-24 03:42:07 -0500

    adding pt2 bonus to R package

--- 366-word-funnel-1/R/funnel/R/part2.R
+++ 366-word-funnel-1/R/funnel/R/part2.R
@@ -14,3 +14,15 @@ r_funnel2 <- function(x, search_func) {
       max(depth)
   loop(x, search_func)
 }
+
+#' Part 2 Bonus
+#'
+#' Find the word that has a funnel depth of 10
+#'
+#' @param words The enable1 list as a character vector.
+#' @param wordset The enable1 list as an external pointer to a C++ unordered set.
+#' @export
+pt2_bonus <- function(words, wordset)
+    for (word in words)
+      if (nchar(word) > 10L && cpp_funnel2(word, wordset) == 10L)
+        return(word)
--- 366-word-funnel-1/R/funnel/tests/testthat/test-part2.R
+++ 366-word-funnel-1/R/funnel/tests/testthat/test-part2.R
@@ -23,6 +23,12 @@ test_that("Rcpp funnel works", {
 })
 
 test_that("Bonus works", {
+  long_words <- enable1[nchar(enable1) > 10]
+  long_words[vapply(long_words, cpp_funnel2, integer(1), word_set) == 10] |>
+    expect_equal("complecting")
+
+  # This is faster because of early exit.
+  pt2_bonus(enable1, word_set) |> expect_equal("complecting")
 })
 
 test_that("Bonus 2 works", {
