c9637762d324b8d876053b3c013a266560473330
pegeler <32426645+pegeler@users.noreply.github.com>
2025-02-09 11:10:52 -0600

    too many braces in derangements

--- 367-derangements/derangements-bigz.r
+++ 367-derangements/derangements-bigz.r
@@ -4,13 +4,10 @@ D <- function(n) {
   x <- as.bigz(0)
   y <- as.bigz(1)
   while (n > 1) {
-    if (n %% 2 == 1)
-      x <- x - y
-    else
-      x <- x + y
+    op <- if (n %% 2 == 1) `-` else `+`
+    x <- op(x, y)
     y <- y * n
     n <- n - 1L
   }
   x
-}
 }
